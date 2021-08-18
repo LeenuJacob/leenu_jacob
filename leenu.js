7a1295d98910e0c8c07c60f65a317da2ff844bec
LeenuJacob <89069690+LeenuJacob@users.noreply.github.com>
2021-08-18 16:36:46 +0530

    Add files via upload

---
+++ leenu.js
@@ -0,0 +1,15 @@
+function makegrey(){
+    document.body.style.background="grey";
+}
+
+function makewhite(){
+    document.body.style.background="white";
+}
+
+function makeblack(){
+    document.body.style.background="black";
+}
+
+function makepink(){
+    document.body.style.background="pink";
+}
